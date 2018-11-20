3e41454d65f0f9dc4ecaf71c49a65c5737af6908
hibikikkk <kyou9712@gmail.com>
2018-11-20 14:05:02 +0900

    a-8完成

---
+++ a-8.py
@@ -0,0 +1,7 @@
+if __name__ == "__main__":
+    user_info = [["Kazuma", 35],
+                 ["Tom", 57],
+                 ["Bob", 77]]
+
+    for user in user_info:
+        print(f"Name: {user[0]}, Age: {user[1]}")
