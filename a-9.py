72546f2b31ec16afe39a32f3f643aea2c256a730
hibikikkk <kyou9712@gmail.com>
2018-11-20 14:07:36 +0900

    a-9完成

---
+++ a-9.py
@@ -0,0 +1,8 @@
+if __name__ == "__main__":
+    kazuma_info = {"first_name": "Kazuma",
+                   "family_name": "Takahashi",
+                   "age": 35}
+
+    print(kazuma_info["first_name"])
+    print(kazuma_info["family_name"])
+    print(kazuma_info["age"])
